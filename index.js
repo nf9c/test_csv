690e57c21e5b1f5c9c4309f38fa214bfe5ca4f4f
nf <coderly@yandex.ru>
2023-05-30 00:14:20 +0300

    Stage4

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ import _ from 'lodash';
 export default function solution(content){
   // BEGIN
   const string = content.split('\n');
-  const keys = string[0].split(',')[2];
+  const passQuantity = string.length - 2;
+  const keys = string[0].split(',');
+  const embarked = _.uniq(string.map((item) => item.split(',')[12]));
+  const passSex = string.map((item) => item.split(',')[5]);
+  const maleQuantity = passSex.filter((item) => item === 'male').length;
+  const femaleQuantity = passSex.filter((item) => item === 'female').length;
+  const survivedQuantity = (string.map((item) => item.split(',')[2]).filter((item) => item === '1').length * 100) / passQuantity;
+  //const sexProportion = 
 
 
 
-  console.log(string.length);
-  console.log(keys, Array.isArray(string), string[1]);
+  console.log(passQuantity);
+  console.log(embarked);
+  console.log(`${maleQuantity}/${femaleQuantity}`);
+  console.log(survivedQuantity);
+  //console.log(keys, maleQuantity, femaleQuantity, embarked, Array.isArray(string), string[55]);
+  
   // END
 };
