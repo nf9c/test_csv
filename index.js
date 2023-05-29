2427be748eecff878bb0c379ba32ef2d674a9a37
nf <coderly@yandex.ru>
2023-05-29 22:40:07 +0300

    Stage1

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import _ from 'lodash';
 
 export default function solution(content){
   // BEGIN
-  console.log(content);
+  const string = content.split('\n');
+  const keys = string[0].split(',')[2];
+
+
+
+  console.log(string.length);
+  console.log(keys, Array.isArray(string), string[1]);
   // END
-}
+};
